598259358aaece4c400512efa11ddf65dc576b0c
Kristopher Johnson <kris@kristopherjohnson.net>
2019-06-13 18:37:03 -0400

    Initial Commit

---
+++ PokerSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PokerSwiftUI
+//
+//  Created by Kristopher Johnson on 6/13/19.
+//  Copyright © 2019 Kristopher Johnson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
